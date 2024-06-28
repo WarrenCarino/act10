927c0d173f47227fb412a1742888c0a322128a6a
Cabrera22A3 <solesourc3@gmail.com>
2024-06-28 21:47:26 +0800

    ahahdadfaf

---
+++ app/src/main/java/com/example/inventory/ui/item/ItemDetailsViewModel.kt
@@ -0,0 +1,44 @@
+package com.example.inventory.ui.item
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.inventory.data.ItemsRepository
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+
+/**
+ * ViewModel to retrieve, update and delete an item from the [ItemsRepository]'s data source.
+ */
+class ItemDetailsViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val itemsRepository: ItemsRepository
+) : ViewModel() {
+
+    val uiState: StateFlow<ItemDetailsUiState>
+        get() = itemsRepository.getItemStream(this.itemId)
+            .filterNotNull()
+            .map {
+                ItemDetailsUiState(itemDetails = it.toItemDetails())
+            }.stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+                initialValue = ItemDetailsUiState()
+            )
+    private val itemId: Int = checkNotNull(savedStateHandle[ItemDetailsDestination.itemIdArg])
+
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+}
+
+/**
+ * UI state for ItemDetailsScreen
+ */
+data class ItemDetailsUiState(
+    val outOfStock: Boolean = true,
+    val itemDetails: ItemDetails = ItemDetails()
+)
---
+++ app/src/main/java/com/example/inventory/ui/navigation/NavigationDestination.kt
@@ -0,0 +1,7 @@
+package com.example.inventory.ui.navigation
+
+interface NavigationDestination {
+    val route: String
+
+    val titleRes: Int
+}
